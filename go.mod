4bcfcd7ad0c4aaa8b5725aedbf7bb3809d9f25c3
crespire <li.simmon@gmail.com>
2022-11-03 14:16:54 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module logger-test
+
+go 1.19
+
+require github.com/crespire/go_logger v0.2.0
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import logger "github.com/crespire/go_logger"
+
+func main() {
+	logger.Log("hey there")
+	logger.Warn("watch out!")
+}
